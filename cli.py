f8e48d437ad678c2f1a308c3f606c9938f42382e
kenchangh <guanhao3797@gmail.com>
2019-04-23 21:02:07 +0800

    added commandline with click

---
+++ cli.py
@@ -0,0 +1,39 @@
+import click
+from github import Github
+import os
+import logging
+
+
+USER_DATA_PATH = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), 'userdata')
+
+
+def init_user_data_dir():
+    if not os.path.exists(USER_DATA_PATH):
+        os.mkdir(USER_DATA_PATH)
+
+
+@click.command()
+@click.option('-t', '--token',
+              prompt='Your GitHub access token', hide_input=True, help="Your personal GitHub access token")
+@click.option('-e', '--exclude', multiple=True, type=str)
+@click.option('-v', '--verbose', type=bool)
+def export_from_github(token, exclude, verbose):
+    init_user_data_dir()
+
+    if verbose:
+        logging.basicConfig(level=logging.DEBUG)
+
+    github = Github(token)
+    repo_names = set(github.list_all_repo_name())
+
+    for exclude_repo in exclude:
+        repo_names.remove(exclude_repo)
+
+    for repo_name in repo_names:
+        # parallelize this work with threading and generators
+        github.download_repo(repo_name, USER_DATA_PATH)
+
+
+if __name__ == '__main__':
+    export_from_github()
--- github.py
+++ github.py
@@ -1,21 +1,23 @@
 import requests
+import os
+
 
 class Github:
     def __init__(self, token):
-        self.token = token    
+        self.token = token
         self.username = self.get_username()
         print(self.username)
-        
+
     def make_req(self, url, payload):
         final_url = "https://api.github.com" + url
         print(f"sending request to {final_url}")
-        headers = {'Authorization':'token %s' % self.token}
+        headers = {'Authorization': 'token %s' % self.token}
         return (requests.get(final_url, headers=headers, params=payload))
-    
+
     def get_username(self):
         result = self.make_req("/user", {}).json()
         return (result['login'])
-    
+
     def list_all_repo(self):
         result = (self.make_req("/user/repos", {})).json()
         for repo in result:
@@ -27,13 +29,22 @@ class Github:
         for repo in result:
             list.append(repo['full_name'])
         return list
-        
-    def download_repo(self,url):
-        with open("test.tar", "wb") as file:
+
+    def normalize_repo_name(self, repo_name):
+        user, repo = repo_name.split('/')
+        normalized = user+'__'+repo
+        return normalized
+
+    def download_repo(self, url, directory):
+        filename = self.normalize_repo_name(url)+'.tar'
+        filepath = os.path.join(directory, filename)
+        with open(filepath, "wb") as file:
             response = self.make_req("/repos/"+url+"/tarball/master", {})
             file.write(response.content)
 
-token = input("Enter your Github token: ")
-g = Github(token)
-list_of_repo_urls = g.list_all_repo_name()
-g.download_repo(list_of_repo_urls[0])
+
+if __name__ == '__main__':
+    token = input("Enter your Github token: ")
+    g = Github(token)
+    list_of_repo_urls = g.list_all_repo_name()
+    g.download_repo(list_of_repo_urls[0])
